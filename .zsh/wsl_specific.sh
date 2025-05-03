cc3918bf0b900d51cb6873351cc161d1e597c1c2
Tim Dortmann <git@tdortman.com>
2025-05-03 20:48:42 +0200

    rely more on ssh-agent

--- .zsh/wsl_specific.sh
+++ .zsh/wsl_specific.sh
@@ -10,20 +10,10 @@ export LIBVA_DRIVERS_PATH=/usr/lib/dri
 export LIBVA_DRIVER_NAME=d3d12
 export VDPAU_DRIVER=d3d12
 
+eval "$(wsl2-ssh-agent)"
+
 if [[ ! -e ~/.local/bin/code ]]; then
     ln -s "/mnt/c/Users/tim/AppData/Local/Programs/Microsoft VS Code/bin/code" ~/.local/bin/code
-fi
-
-if [[ ! -e ~/.local/bin/ssh ]]; then
-    ln -s "/mnt/c/windows/system32/openssh/ssh.exe" ~/.local/bin/ssh
-fi
-
-if [[ ! -e ~/.local/bin/ssh-add ]]; then
-    ln -s "/mnt/c/windows/system32/openssh/ssh-add.exe" ~/.local/bin/ssh-add
-fi
-
-if [[ ! -e ~/.local/bin/op-ssh-sign-wsl ]]; then
-    ln -s "/mnt/c/Users/tim/AppData/Local/1Password/app/8/op-ssh-sign-wsl" ~/.local/bin/op-ssh-sign-wsl
 fi
 
 alias explorer.exe="/mnt/c/windows/explorer.exe"
