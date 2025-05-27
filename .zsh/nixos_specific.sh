8ebfda6a5336bfad55374f5c1444824e4c5d67e8
Tim Dortmann <git@tdortman.com>
2025-05-27 22:02:30 +0000

    update system to nixOS 25.05

--- .zsh/nixos_specific.sh
+++ .zsh/nixos_specific.sh
@@ -1,8 +1,8 @@
 export NIXPKGS_ALLOW_UNFREE=1
 
-CUDA_ROOT=$(nix --extra-experimental-features 'nix-command flakes' eval --impure --raw github:NixOS/nixpkgs/nixos-unstable#cudaPackages.cudatoolkit)
+#CUDA_ROOT=$(nix --extra-experimental-features 'nix-command flakes' eval --impure --raw github:NixOS/nixpkgs/nixos-unstable#cudaPackages.cudatoolkit)
 
-export PATH="$CUDA_ROOT/bin:$PATH"
+#export PATH="$CUDA_ROOT/bin:$PATH"
 
 alias nixos-switch="sudo nixos-rebuild switch --flake $HOME/dotfiles/nix-config"
 alias flake-update="sudo nix flake update --flake $HOME/dotfiles/nix-config"
--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
