88ea7eabc97a08f33f2c8bcf84cb511c205e7a99
Tim Dortmann <git@tdortman.com>
2025-07-09 19:05:25 +0000

    update system flake

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -2,7 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
