559ca7940c1be4513dcd068aa66bb6f28857017b
Tim Dortmann <git@tdortman.com>
2025-07-15 03:45:51 +0200

    symlink bat to scoop too

--- home/.chezmoiscripts/run_symlink-bat.ps1
+++ home/.chezmoiscripts/run_symlink-bat.ps1
@@ -1,7 +1,14 @@
 if ($IsWindows) {
     $src = Join-Path $Env:USERPROFILE '.config\bat'
     $dst = Join-Path $Env:APPDATA 'bat'
+    $dst_scoop = Join-Path $Env:USERPROFILE 'scoop\persist\bat'
     if (-Not (Test-Path $dst)) {
         New-Item -ItemType Junction -Path $dst -Value $src
+    }
+    if (-Not (Test-Path $dst_scoop) -or ((Get-Item $dst_scoop).LinkType -ne 'Junction')) {
+        if (Test-Path $dst_scoop) {
+            Remove-Item $dst_scoop -Force
+        }
+        New-Item -ItemType Junction -Path $dst_scoop -Value $src
     }
 }
