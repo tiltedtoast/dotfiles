cdd3b276da86b07aba7c3d8e315c05b68e4a8555
Tim Dortmann <git@tdortman.com>
2025-08-18 16:16:14 +0200

    printing

--- nix/hosts/desktop/default.nix
+++ nix/hosts/desktop/default.nix
@@ -31,6 +31,20 @@
     };
   };
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      cups-filters
+      cups-browsed
+    ];
+  };
+
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
