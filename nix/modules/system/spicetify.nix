8035a954182ab485e6a4feebedd34f7a8fccddc0
Tim Dortmann <git@tdortman.com>
2025-09-19 02:52:44 +0200

    fix spicetify extensions not being found by the cli

--- nix/modules/system/spicetify.nix
+++ nix/modules/system/spicetify.nix
@@ -9,39 +9,45 @@
 
       enabledExtensions = with spicePkgs.extensions; [
         {
-          src = pkgs.fetchFromGitHub {
-            owner = "41pha1";
-            repo = "spicetify-extensions";
-            rev = "5015b9122a9c39274bdc7507e1de4fec0cc20f95";
-            hash = "sha256-JkkIs40Kp57kLTcb95WIgfHDoCP/LHZ3TmdP1w1e1OY=";
-          };
-          name = "romaji-lyrics/romaji_lyrics.js";
+          src =
+            (pkgs.fetchFromGitHub {
+              owner = "41pha1";
+              repo = "spicetify-extensions";
+              rev = "5015b9122a9c39274bdc7507e1de4fec0cc20f95";
+              hash = "sha256-JkkIs40Kp57kLTcb95WIgfHDoCP/LHZ3TmdP1w1e1OY=";
+            })
+            + "/romaji-lyrics";
+          name = "romaji_lyrics.js";
         }
         {
-          src = pkgs.fetchFromGitHub {
-            owner = "theblockbuster1";
-            repo = "spicetify-extensions";
-            rev = "b156cfcf9ed603e131ac2241bf00d7010e1c53e9";
-            hash = "sha256-tNgIXOTR1Wzl6u7bvcgbx4Gqe8/UfKXZuoXqw666G7E=";
-          };
-          name = "CoverAmbience/CoverAmbience.js";
-        }
-        {
-          src = pkgs.fetchFromGitHub {
-            owner = "resxt";
-            repo = "spicetify-extensions";
-            rev = "75bd17ba1c9a19730f14529fb18857d7b9c7c12e";
-            hash = "sha256-+Th5o00c3Y8U+Y/RGmRSkWWp97YCoCJmoESFLZf9dwM=";
-          };
-          name = "startup-page/dist/startup-page.js";
+          src =
+            (pkgs.fetchFromGitHub {
+              owner = "resxt";
+              repo = "spicetify-extensions";
+              rev = "75bd17ba1c9a19730f14529fb18857d7b9c7c12e";
+              hash = "sha256-+Th5o00c3Y8U+Y/RGmRSkWWp97YCoCJmoESFLZf9dwM=";
+            })
+            + "/startup-page/dist";
+          name = "startup-page.js";
         }
         fullAlbumDate
         shuffle
         seekSong
+        keyboardShortcut
+        fullAlbumDate
       ];
 
       enabledCustomApps = with spicePkgs.apps; [
         historyInSidebar
+        marketplace
+
+        {
+          src = pkgs.fetchzip {
+            url = "https://github.com/harbassan/spicetify-apps/releases/download/stats-v1.1.2/spicetify-stats.release.zip";
+            hash = "sha256-lIWAJDO/2fZEzBmK79wdB9H78+4A1xOw90Zy4e4ql4s=";
+          };
+          name = "index.js";
+        }
       ];
 
       enabledSnippets = with spicePkgs.snippets; [
@@ -50,6 +56,7 @@
         prettyLyrics
         fixMainViewWidth
         roundedImages
+        removeConnectBar
       ];
 
       theme = spicePkgs.themes.lucid;
