a92eac246c1579584dfaf7926e31416fe4dc5e1a
Tim Dortmann <git@tdortman.com>
2025-11-27 10:04:50 +0100

    pin home-manager to 25.11 branch

--- nix/flake.nix
+++ nix/flake.nix
@@ -7,7 +7,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
