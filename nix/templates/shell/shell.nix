6d332b48e82016b7c86914eafa0f5a9c7aab6971
Tim Dortmann <git@tdortman.com>
2025-12-02 23:59:25 +0100

    basic template for shell.nix

--- nix/templates/shell/shell.nix
+++ nix/templates/shell/shell.nix
@@ -3,12 +3,16 @@ let
   pkgs = import nixpkgs {
     config.allowUnfree = true;
   };
-in
 
-pkgs.mkShell {
   buildInputs = with pkgs; [
   ];
 
   nativeBuildInputs = with pkgs; [
   ];
+in
+
+pkgs.mkShell {
+  inherit buildInputs nativeBuildInputs;
+
+  LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (buildInputs ++ nativeBuildInputs)}";
 }
