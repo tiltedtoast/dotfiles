25b2c3c89755530c70902d19fa7e5829bf6f155d
Tim Dortmann <git@tdortman.com>
2025-12-27 02:09:56 +0100

    remove old nvim config

--- home/dot_config/nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-vim.api.nvim_command('set shiftwidth=4')
-vim.api.nvim_command('set tabstop=4')
