043d80595aa3970af23d478780f76e8babe5bfe1
Tim Dortmann <git@tdortman.com>
2026-02-13 19:41:44 +0100

    change to wow64

--- nix/modules/nixos/gaming/default.nix
+++ nix/modules/nixos/gaming/default.nix
@@ -25,7 +25,7 @@ in
 
     programs.gamemode.enable = true;
     environment.systemPackages = with pkgs; [
-      wineWowPackages.stable
+      wineWow64Packages.stable
       mangohud
       protonup-qt
       lutris
